eab638e1f737ce559b475fcaf99548fed8b5eb37
einadid <emamulislamnadid_cse30d@portcity.edu.bd>
2025-12-08 23:15:23 +0600

    Fixed routing and auth loading issue

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -3,28 +3,31 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from './useAuth';
 
-const axiosSecure = axios.create({
+export const axiosSecure = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true, // এটি থাকতে হবে
+  withCredentials: true,
 });
 
-// Interceptor
 const useAxiosSecure = () => {
   const { logOut } = useAuth();
   const navigate = useNavigate();
 
   useEffect(() => {
-    axiosSecure.interceptors.response.use(
+    const interceptor = axiosSecure.interceptors.response.use(
       res => res,
       async error => {
-        // যদি 401 বা 403 আসে, লগআউট করে দিন
-        if (error.response.status === 401 || error.response.status === 403) {
+        if (error.response?.status === 401 || error.response?.status === 403) {
           await logOut();
           navigate('/login');
         }
         return Promise.reject(error);
       }
     );
+
+    // ✅ clean up (important)
+    return () => {
+      axiosSecure.interceptors.response.eject(interceptor);
+    };
   }, [logOut, navigate]);
 
   return axiosSecure;
